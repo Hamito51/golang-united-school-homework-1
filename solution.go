faeabbf67925105c39a8943cc396e493de836016
Aleksei_Chulei <aleksei_chulei@epam.com>
2022-02-13 22:54:48 +0300

    change func

--- solution.go
+++ solution.go
@@ -1,13 +1,10 @@
 package solution
 
 import (
-	"fmt"
-
 	"github.com/kyokomi/emoji/v2"
 )
 
-func GetMessage() {
+func GetMessage() string {
 
-	helloMessage := emoji.Sprint("Hello :world_map:!")
-	fmt.Println(helloMessage)
+	return emoji.Sprint("Hello :world_map:!")
 }
