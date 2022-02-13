b2d48df678a98d3d1e13ee464c214e3e09e8babb
Aleksei_Chulei <aleksei_chulei@epam.com>
2022-02-13 22:51:55 +0300

    test

---
+++ solution.go
@@ -0,0 +1,13 @@
+package solution
+
+import (
+	"fmt"
+
+	"github.com/kyokomi/emoji/v2"
+)
+
+func GetMessage() {
+
+	helloMessage := emoji.Sprint("Hello :world_map:!")
+	fmt.Println(helloMessage)
+}
